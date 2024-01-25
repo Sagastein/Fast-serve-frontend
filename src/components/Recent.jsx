9eefa268c3811e1df6d9a52b41ea58dc3c976eed
Sagastein <ishimwe.sage.3@gmail.com>
2024-01-25 18:55:55 +0200

    Add Recent component for displaying recent transactions

---
+++ src/components/Recent.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import useSWR from "swr";
+import { Spinner } from "@nextui-org/react";
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+
+function Recent() {
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:4000/api/transaction/recent",
+    fetcher
+  );
+  if (isLoading)
+    return (
+      <div className="my-4 p-12 text-center">
+        <Spinner />
+      </div>
+    );
+  if (error)
+    return (
+      <div className="my-4 py-12 mx-auto">Error connectin to the server</div>
+    );
+
+  return (
+    <main className="grid bg-transparent space-y-4">
+      {data && data.length ? (
+        data.map((item) => (
+          <section
+            key={item.id}
+            className="bg-white justify-between  space-y-3.5 p-2 py-2 leading-7  hover:shadow-lg hover:scale-95 cursor-pointer  rounded-md shadow"
+          >
+            <div className="flex flex-wrap justify-between">
+              <h1 className="capitalize font-medium">{item.username}</h1>
+              <span className="text-xs font-thin">{item.timeAgo}</span>
+            </div>
+            <div className="flex flex-wrap justify-between">
+              <span className="text-xs font-mono font-medium">
+                Bal:{item.accountBalance?.toLocaleString()}
+                <code>Rwf</code>
+              </span>
+              <span className="text-xs font-light capitalize text-gray-500">
+                {item.transactionType} {item.amount?.toLocaleString()} Rwf
+              </span>
+            </div>
+          </section>
+        ))
+      ) : (
+        <div className="capitalize text-center border py-8 rounded-md bg-green-700/30 text-green-500">
+          no recent data 📄
+        </div>
+      )}
+    </main>
+  );
+}
+
+export default Recent;
