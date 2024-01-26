6a955080c69fffae4637288be5b2bd067a244cc6
Sagastein <ishimwe.sage.3@gmail.com>
2024-01-26 22:09:29 +0200

    Add Adduser component and getid utility function

---
+++ src/components/getid.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/rules-of-hooks */
+import axios from "axios";
+import useSWR from "swr";
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+var nana;
+export const id = () => {
+  const { data, error } = useSWR("/api/ca", fetcher);
+
+  if (error) return (nana = `server error`);
+
+  return (nana = data);
+};
